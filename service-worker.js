1ea3c59e43c208ad3b705d73f9b4e2d768918172
hnasik <hnasik@gmail.com>
2026-08-05 11:45:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -13,10 +13,12 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
+
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
+
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(
@@ -28,11 +30,19 @@ self.addEventListener("activate", (event) => {
       )
     )
   );
+
   self.clients.claim();
+
 });
 
 self.addEventListener("fetch", (event) => {
+
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+
+      return response || fetch(event.request);
+
+    })
   );
+
 });
